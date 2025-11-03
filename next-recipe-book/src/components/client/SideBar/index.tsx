bb7ce3e813b74a049c8e7f57d78d3d0d649efe5c
Hoang Le <hoang.le@asnet.com.vn>
2025-11-03 18:15:29 +0700

    Create Header component

---
+++ next-recipe-book/src/components/client/SideBar/index.tsx
@@ -0,0 +1,5 @@
+const SideBar = () => {
+  return <div>SideBar Component</div>;
+};
+
+export default SideBar;
---
+++ next-recipe-book/src/components/server/Header/index.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="bg-primary">
+      <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href="/recipes">
+          <h1 className="text-xl font-semibold text-white tracking-wide">
+            The Next Recipe Book
+          </h1>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
