51e4fdadf6a5942738fb2e35aac3f65b7c684026
Hoang Le <hoang.le@asnet.com.vn>
2025-11-04 10:56:16 +0700

    Create SideBar component

--- next-recipe-book/src/app/layout.tsx
+++ next-recipe-book/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
 import Header from "@/components/server";
-import SideBar from "@/components/client";
+import { SideBar } from "@/components/client";
 
 export const metadata: Metadata = {
   title: "Next Recipe Book",
@@ -17,8 +17,12 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         <Header />
-        <SideBar />
-        {children}
+        <div className="flex flex-col-reverse md:flex-row gap-8 px-10 py-10">
+          <div className="md:w-5/12 lg:w-4/12 xl:w-3/12 2xl:w-2/12">
+            <SideBar />
+          </div>
+          <div className="md:w-7/12 lg:w-8/12 xl:w-9/12 2xl:w-10/12">{children}</div>
+        </div>
       </body>
     </html>
   );
--- next-recipe-book/src/components/client/SideBar/index.tsx
+++ next-recipe-book/src/components/client/SideBar/index.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import { Input, FilterButton, Button } from "@/components/client";
+import { CUISINE_TYPE_OPTIONS } from "@/constants/";
+
 const SideBar = () => {
-  return <div>SideBar Component</div>;
+  return (
+    <div className="flex border border-gray-100 flex-col gap-4 bg-background shadow-md rounded-lg">
+      <div className="px-4 py-14">
+        <Input id="11" value="" onChange={() => {}} />
+      </div>
+      <div className="px-4 pb-10">
+        <FilterButton
+          title="Cuisine Types"
+          filterList={CUISINE_TYPE_OPTIONS}
+          onClick={() => {}}
+        />
+      </div>
+      <div className="border-t border-gray-300 px-4 py-6">
+        <Button title="Add New Recipe" />
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
