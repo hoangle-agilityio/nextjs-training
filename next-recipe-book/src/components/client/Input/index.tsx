97748b85a5703c5ace9d0022d8894ed741966811
Hoang Le <hoang.le@asnet.com.vn>
2025-11-04 17:07:09 +0700

    Update debounce for Input component

--- next-recipe-book/src/components/client/Input/index.tsx
+++ next-recipe-book/src/components/client/Input/index.tsx
@@ -1,14 +1,36 @@
 "use client";
 
 import { SearchIcon } from "@/components/icons";
+import { DEBOUNCE_DELAY } from "@/constants";
+import { useState, useEffect } from "react";
 
 interface InputProps {
   id: string;
   value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange: (value: string) => void;
 }
 
 const Input = ({ id, value, onChange }: InputProps) => {
+  const [localValue, setLocalValue] = useState(value);
+
+  useEffect(() => {
+    setLocalValue(value);
+  }, [value]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (localValue !== value) {
+        onChange(localValue);
+      }
+    }, DEBOUNCE_DELAY);
+
+    return () => clearTimeout(timer);
+  }, [localValue]);
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLocalValue(e.target?.value || "");
+  };
+
   return (
     <div className="flex gap-2 items-center border border-gray-400 p-3 rounded-md">
       <SearchIcon />
@@ -16,9 +38,10 @@ const Input = ({ id, value, onChange }: InputProps) => {
         key={id}
         id={id}
         type="text"
+        className="px-2 outline-none"
         placeholder="Search"
-        value={value}
-        onChange={onChange}
+        value={localValue}
+        onChange={handleChangeInput}
       />
     </div>
   );
